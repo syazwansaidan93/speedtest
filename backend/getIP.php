4fd0729a6e5e0e64e8f926849d53b335b860f8d4
syazwansaidan93 <78026725+syazwansaidan93@users.noreply.github.com>
2023-06-20 13:20:24 +0800

    Update getIP.php

--- backend/getIP.php
+++ backend/getIP.php
@@ -195,10 +195,7 @@ function sendResponse(
     $ipInfo = null,
     $rawIspInfo = null
 ) {
-    $processedString = $ip;
-    if (is_string($ipInfo)) {
-        $processedString .= ' - '.$ipInfo;
-    }
+    
 
     if (
         is_array($rawIspInfo)
